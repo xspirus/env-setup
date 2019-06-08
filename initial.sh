8c2ee1eee8a184e9dd3d85ef4d18b163e2810a66
Spiros Dontas <spirosdontas@gmail.com>
2019-06-08 18:20:36 +0300

    Add functions

--- initial.sh
+++ initial.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+install_trizen () {
+    git clone https://aur.archlinux.org/trizen.git
+    cd trizen
+    makepkg -d
+    sudo -k -S pacman -U *.pkg.tar.gz --noconfirm --needed <<< $PASSWORD
+    cd ..
+    rm -rf trizen
+}
+
 source .env
 printf "$PASSWORD\n[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch\n" | sudo -k -S tee -a /etc/pacman.conf
-git clone https://aur.archlinux.org/trizen.git
-cd trizen
-makepkg -d
-sudo -k -S pacman -U *.pkg.tar.gz --noconfirm --needed <<< $PASSWORD
-cd ..
-rm -rf trizen
 sudo -k -S pacman -Sy <<< $PASSWORD
 sudo -k -S pacman -S python python-pip git --noconfirm --needed <<< $PASSWORD
+install_trizen
 curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 export PATH=$PATH:$EXTRA_PATHS
 git clone https://github.com/xspirus/env-setup.git
