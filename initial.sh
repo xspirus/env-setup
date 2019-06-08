bba37dd8652c1714ea86ccfbb3ff7caa0bc5ab8d
Spiros Dontas <spirosdontas@gmail.com>
2019-06-08 19:51:59 +0300

    Add debian initial script

--- initial.sh
+++ initial.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+PYENV_STRING="\nexport PATH=\"$HOME/.pyenv/bin:\$PATH\"\n"
+PYENV_STRING="${PYENV_STRING}eval \"\$(pyenv init -)\"\n"
+PYENV_STRING="${PYENV_STRING}eval \"\$(pyenv virtualenv-init -)\""
+
+source .env
+
+install_pyenv () {
+    sudo -k -S apt-get --yes install make build-essential gcc g++ llvm \
+libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
+libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev \
+liblzma-dev python-openssl git <<< $PASSWORD
+    curl https://pyenv.run | bash
+    echo -e $PYENV_STRING | tee -a $HOME/.bashrc
+    export PATH=$HOME/.pyenv/bin:$PATH
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    pyenv install 2.7.16
+    pyenv install 3.7.3
+    pyenv global 3.7.3 2.7.16
+}
+
+install_poetry () {
+    curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+    echo -e "\nexport PATH=\$HOME/.poetry/bin:\$PATH" | tee -a $HOME/.bashrc
+    export PATH=$HOME/.poetry/bin:$PATH
+    poetry config settings.virtualenvs.in-project true
+}
+
 install_setup () {
     git clone https://github.com/xspirus/env-setup.git
     cp .env env-setup/.env
@@ -7,12 +35,11 @@ install_setup () {
     git branch arch/debian
     git pull origin arch/debian
     poetry install
-    poetry run setup
+    poetry run debian
 }
 
-source .env
-sudo -k apt-get update <<< $PASSWORD
-sudo -k apt-get install python python-pip python3 python3-pip git -y <<< $PASSWORD
-curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
-export PATH=$PATH:/home/$USER/.poetry/bin
+sudo -k -S apt-get update <<< $PASSWORD
+sudo -k -S apt-get --yes upgrade <<< $PASSWORD
+install_pyenv
+install_poetry
 install_setup
---
+++ setup/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+
+__version__ = "0.1.0"
+__root__ = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+dotenv_path = os.path.join(__root__, ".env")
+load_dotenv(dotenv_path=dotenv_path)
