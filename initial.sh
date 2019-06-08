bfc84e7eaeb72a558e3b915fa79acc0df3b3a54a
Spiros Dontas <spirosdontas@gmail.com>
2019-06-08 17:34:57 +0300

    Small fix

--- initial.sh
+++ initial.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 source .env
-sudo -k
-echo "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch" | { echo $PASSWORD; cat -; } | sudo -S tee -a /etc/pacman.conf
-cat /etc/pacman.conf
-sudo -S pacman -Sy <<< $PASSWORD
-sudo -k
-sudo -S pacman -S python python-pip git --noconfirm --needed <<< $PASSWORD
+printf "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch\n" | { echo $PASSWORD; cat -; } | sudo -k -S tee -a /etc/pacman.conf
+sudo -k -S pacman -Sy <<< $PASSWORD
+sudo -k -S pacman -S python python-pip git --noconfirm --needed <<< $PASSWORD
 curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 export PATH=$PATH:$EXTRA_PATHS
 git clone https://github.com/xspirus/env-setup.git
